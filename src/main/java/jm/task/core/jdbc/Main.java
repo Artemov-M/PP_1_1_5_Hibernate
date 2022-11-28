e0d01d46be175751068e68e47ddeb101c1b0defb
Mikhail Artemov <artemov.m.p@gmail.com>
2022-11-28 23:03:59 +0400

    Исправления после проверки. 1. UserDaoJDBCImpl userDao = new UserDaoJDBCImpl(); - использовать интерфейс 2. писать запросы сразу в методы 3. var userService = new UserServiceImpl(); - использовать интерфейс

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,12 +1,13 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
-        var userService = new UserServiceImpl();
+        UserService userService = new UserServiceImpl();
 
         userService.createUsersTable();
 
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -3,43 +3,13 @@ package jm.task.core.jdbc.dao;
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-
-    private final String CREATE_TABLE = """
-            CREATE TABLE IF NOT EXISTS user (
-                id BIGINT PRIMARY KEY AUTO_INCREMENT,
-                name VARCHAR(100) NOT NULL,
-                last_name VARCHAR(100) NOT NULL,
-                age TINYINT NOT NULL CHECK (age >= 0)
-            )
-            """;
-
-    private final String DROP_TABLE = """   
-            DROP TABLE IF EXISTS user;
-            """;
-
-    private final String SAVE_USER = """
-            INSERT INTO user (name, last_name, age)
-            VALUES (?, ?, ?)
-            """;
-
-    private final String REMOVE_USER_BY_ID = """
-            DELETE FROM user
-            WHERE id = ?
-            """;
-
-    private final String GET_ALL_USERS = """
-            SELECT id, name, last_name, age
-            FROM user;
-            """;
-
-    private final String CLEAN_USERS_TABLE = """
-            TRUNCATE TABLE user;
-            """;
 
     public UserDaoJDBCImpl() {
     }
@@ -47,7 +17,16 @@ public class UserDaoJDBCImpl implements UserDao {
     public void createUsersTable() {
         try (Connection connection = Util.getConnection();
              var statement = connection.createStatement()) {
-            statement.execute(CREATE_TABLE);
+            statement.execute(
+            """
+                    CREATE TABLE IF NOT EXISTS user (
+                        id BIGINT PRIMARY KEY AUTO_INCREMENT,
+                        name VARCHAR(100) NOT NULL,
+                        last_name VARCHAR(100) NOT NULL,
+                        age TINYINT NOT NULL CHECK (age >= 0)
+                    )
+                """
+            );
         } catch (SQLException ignore) {
         }
     }
@@ -55,14 +34,15 @@ public class UserDaoJDBCImpl implements UserDao {
     public void dropUsersTable() {
         try (Connection connection = Util.getConnection();
              var statement = connection.createStatement()) {
-            statement.execute(DROP_TABLE);
+            statement.execute("DROP TABLE IF EXISTS user;");
         } catch (SQLException ignore) {
         }
     }
 
     public void saveUser(String name, String lastName, byte age) {
         try (Connection connection = Util.getConnection();
-             var statement = connection.prepareStatement(SAVE_USER, Statement.RETURN_GENERATED_KEYS)) {
+             var statement = connection.prepareStatement(
+                     "INSERT INTO user (name, last_name, age) VALUES (?, ?, ?)")) {
             statement.setString(1, name);
             statement.setString(2, lastName);
             statement.setByte(3, age);
@@ -73,7 +53,8 @@ public class UserDaoJDBCImpl implements UserDao {
 
     public void removeUserById(long id) {
         try (Connection connection = Util.getConnection();
-             var statement = connection.prepareStatement(REMOVE_USER_BY_ID)) {
+             var statement = connection.prepareStatement(
+                     "DELETE FROM user WHERE id = ?")) {
             statement.setLong(1, id);
             statement.executeUpdate();
         } catch (SQLException ignore) {
@@ -84,7 +65,8 @@ public class UserDaoJDBCImpl implements UserDao {
         List<User> result = new ArrayList<>();
         try (Connection connection = Util.getConnection();
              var statement = connection.createStatement()) {
-            var resultSet = statement.executeQuery(GET_ALL_USERS);
+            var resultSet = statement.executeQuery(
+                    "SELECT id, name, last_name, age FROM user");
             while (resultSet.next()) {
                 result.add(createUser(resultSet));
             }
@@ -96,12 +78,12 @@ public class UserDaoJDBCImpl implements UserDao {
     public void cleanUsersTable() {
         try (Connection connection = Util.getConnection();
              var statement = connection.createStatement()) {
-            statement.execute(CLEAN_USERS_TABLE);
+            statement.execute("TRUNCATE TABLE user");
         } catch (SQLException ignore) {
         }
     }
 
-    private User createUser (ResultSet resultSet) throws SQLException {
+    private User createUser(ResultSet resultSet) throws SQLException {
         User user = new User();
         user.setId(resultSet.getLong("id"));
         user.setName(resultSet.getString("name"));
